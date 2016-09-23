532d139f905807b55c84ed734eaa322d3b115035
rgadele2020 <rgadela2020@gmail.com>
2016-09-23 14:32:18 +1000

    Generated Files

--- devicecore/gensrc/com/device/core/constants/GeneratedDeviceCoreConstants.java
+++ devicecore/gensrc/com/device/core/constants/GeneratedDeviceCoreConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 21/09/2016 2:57:47 PM                       ---
+ * --- Generated at 23/09/2016 1:29:21 PM                       ---
  * ----------------------------------------------------------------
  */
 package com.device.core.constants;
